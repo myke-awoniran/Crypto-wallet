068c70baf8df400566f288b5f9ed086069197b1f
Myke <awoniranopeyemi@gmail.com>
2022-08-27 15:46:19 +0100

    Delete build directory

--- build/routes/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const express_1 = require("express");
-const controller_1 = require("../controllers/decorators/controller");
-const router = (0, express_1.Router)();
-// router.get('/sign-in', (req: Request, res: Response) => {
-//     res.send('hello, crypto server is responding')
-// })
-router.use(controller_1.router);
-module.exports = router;
